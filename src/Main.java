88b6c57eb80a53c28e1d8181905d559e60d6d105
TCaeiro <145690383+TCaeiro@users.noreply.github.com>
2023-09-22 21:16:49 +0100

    Update Main.java
    teste1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import contactBook.ContactBook;
 
 import java.util.Scanner;
 
-
+//#vski
 public class Main {
     //Constantes que definem os comandos
     public static final String ADD_CONTACT    = "AC";
